6bfb68ae921382e57c1d0621ae60e7e4a4d69150
Jamaal Lawson <maaliemaal@gmail.com>
2021-08-09 10:03:30 -0400

    added routes to the homepage, notes page, and a path to the json file. Then added a post route to post the notes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { clog } = require('./middleware/clog');
+const { v4: uuidv4 } = require('uuid');
+const { text } = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 
@@ -27,9 +29,61 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.post('/api/notes', (req,res) => {
-
+app.get('/api/notes', (req,res) => {
+  res.sendFile(path.join(__dirname, '/db/db.json'))
 }),
+
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
+
+  // Destructuring assignment for the items in req.body
+  const { title,text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+     title,
+     text,
+     note_id: uuidv4(),
+    };
+
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsednotes = JSON.parse(data);
+
+        // Add a new review
+        parsednotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsednotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in posting review');
+  }
+});
 
 
 app.listen(PORT, () =>
